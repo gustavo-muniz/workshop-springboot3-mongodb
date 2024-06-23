4233bf7ac3ad6f56077519b641c299234bce7cf4
Gustavo Muniz <gustamunizbr@gmail.com>
2024-06-23 16:36:59 -0300

    Project created

---
+++ src/test/java/com/munizdev/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munizdev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
